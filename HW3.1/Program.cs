8ac1b6b9f52f8671aebf6bcab9c2c73652d2ae5a
Beslan <biozdoev@gmail.com>
2022-10-12 16:24:02 +0300

    First Task

---
+++ HW3.1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Для того, чтобы показать кубы всех чисел от 1 до N, задайте число N:");
+int numberA = int.Parse(Console.ReadLine());
+int count = 1;
+int numberB = 1;
+while (count <= numberA)
+{
+int numberC = numberB*numberB*numberB;
+Console.WriteLine($"{numberC} является кубом числа {numberB}");
+numberB++;
+count++;
+}
