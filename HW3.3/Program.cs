21fcdec8cf187cad1e7eebe08e6b1dedf38d4c71
Beslan <biozdoev@gmail.com>
2022-10-12 16:58:54 +0300

    Third Task

---
+++ HW3.3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Для того, чтобы вычислить произведение чисел от 1 до N, задайте число N:");
+int numberA = int.Parse(Console.ReadLine());
+int count = 1;
+int prod = 1;
+while (numberA > 0 && count <= numberA)
+            {
+                prod = prod * count;
+                count = count + 1;
+            }
+
+ Console.WriteLine(prod);
