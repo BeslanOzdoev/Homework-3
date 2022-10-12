e69aa658c82dd7282cf5f7eddfac6a1afb792174
Beslan <biozdoev@gmail.com>
2022-10-12 16:46:15 +0300

    Second Task

---
+++ HW3.2/Program.cs
@@ -0,0 +1,10 @@
+﻿ 
+ int numberA = int.Parse(Console.ReadLine());
+            int sum = 0;
+            while (numberA > 0)
+            {
+                sum = sum + numberA % 10;
+                numberA = numberA / 10 ;
+            }
+
+ Console.WriteLine(sum);
